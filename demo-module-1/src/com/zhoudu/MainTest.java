e6bc9cf4851cb547eff62e6876efcddd0718f468
zhouduforgit <1974788603@qq.com>
2020-12-15 08:21:45 +0800

    这是在MainTest中增加了change的信息

--- demo-module-1/src/com/zhoudu/MainTest.java
+++ demo-module-1/src/com/zhoudu/MainTest.java
@@ -3,5 +3,6 @@ package com.zhoudu;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("cgebf chenga change");
     }
 }
